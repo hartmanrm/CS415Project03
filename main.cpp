3191be8b72edc26643c45d09621a0f8d5f6347d1
czeliger <carolinezeliger04@gmail.com>
2026-04-12 20:29:44 -0700

    New project and framework
    Created classes and files for each type of trie. Added a rough plan for what needs to be completed in the main.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "StandardTrie.h"
+#include "TernarySearchTree.h"
+
+/*
+Open file
+Read file word by word ignoring spaces
+Ignore punctuation and make everything lowercase
+    Insert word into Trie
+    Calculate space needed for each implementation
+Calculate time taken to build Trie
+Print info
+
+Autocomplete
+    print every potential word that uses the prefix
+    calculate search time
+    calculate time to find autocomplete
+
+*/
